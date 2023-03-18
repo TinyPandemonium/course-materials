0c18a78f588d8ee4633c33dcca1c816024979146
uncannysmile <sprinklingsunshower@gmail.com>
2023-03-17 17:03:21 -0700

    start of building the infrastructure

---
+++ unit-2/week-1/d5-intro-mongoose/student_notes/mongoose-movies/routes/movies.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var moviesCtrl = require('../controllers/movies')
+
+// GET movies/new 
+// localhost:3000/movies/new
+// All routes start with /movies
+// /movies is already taken care of in server.js
+router.get('/new', moviesCtrl.new)
+
+// POST /movies
+// localhost:3000/movies
+router.post('/', moviesCtrl.create)
+
+
+module.exports = router;
